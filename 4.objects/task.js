ee9e83124646e2cf00065944d958bd0bf1364e24
Alexey Pozhalov <pozhalov7@mail.ru>
2023-05-08 19:19:09 +0300

    Minor edits

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,33 +4,33 @@ function Student(name, gender, age) {
     this.age = age;
     this.marks = [];
 }
-const student1 = new Student('Zhanna', 'female', 23)
-const student2 = new Student('Alex', 'male', 33)
-const student3 = new Student('Masha', 'female', 27)
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if(!this.marks){
+    if (!this.marks) {
         console.log(`Студент ${this.name} отчислен!`);
         return
     }
-  this.marks.push(...marks);
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  if(!this.marks || this.marks.length === 0){
-      return 0
-  }
-  const sum = this.marks.reduce((a,b)=>a+b,0);
-  return  sum / this.marks.length
+    if (!this.marks || this.marks.length === 0) {
+        return 0
+    }
+    const sum = this.marks.reduce((a, b) => a + b, 0);
+    return sum / this.marks.length
 
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.exclude = reason
+    this.excluded = reason
+    delete this.subject;
+    delete this.marks;
 }
+const student1 = new Student('Zhanna', 'female', 23)
+const student2 = new Student('Alex', 'male', 33)
+const student3 = new Student('Masha', 'female', 27)
