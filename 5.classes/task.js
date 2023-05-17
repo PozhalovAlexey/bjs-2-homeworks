8241c43e3997d5aa7f9a6b65c11114146c1a22ac
Alexey Pozhalov <pozhalov7@mail.ru>
2023-05-17 16:48:24 +0300

    Completed homework

---
+++ 5.classes/task.js
@@ -0,0 +1,126 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set setState(state) {
+    if (state < 0) {
+      return (this.state = 0);
+    } else if (state > 100) {
+      return (this.state = 100);
+    } else {
+      return (this.state = state);
+    }
+  }
+
+  get currentState() {
+    return this.state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+    this.author = author;
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "book";
+    this.author = author;
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.currentState > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    for (let i = 0; i < this.books.length; i++) {
+      const book = this.books[i];
+      if (book[type] === value) {
+        return book;
+      }
+    }
+    return null;
+  }
+
+  giveBookByName(bookName) {
+    for (let i = 0; i < this.books.length; i++) {
+      const book = this.books[i];
+      if (book.name === bookName) {
+        this.books.splice(i, 1);
+        return book;
+      }
+    }
+    return null;
+  }
+}
+
+const library = new Library("Библиотека имени Белинского");
+
+const eugeneOnegin = new Book("А.С. Пушкин", "Eugene Onegin", 1833, 268);
+const richDad = new Book("Роберт Кийосаки", "Богатый папа, бедный папа", 1997, 336);
+const cleanCode = new Magazine("Роберт Мартин", "Чистый код", 2008, 464);
+const pridePrejudice = new Book("Джейн Остин", "Гордость и предубеждение", 1813, 432);
+const damagedBook = new Book("Автор", "Название", 1919, 200);
+const machineTime = new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138);
+
+library.addBook(new FantasticBook(eugeneOnegin));
+library.addBook(new Book(richDad));
+library.addBook(new Book(cleanCode));
+library.addBook(new NovelBook(pridePrejudice));
+library.addBook(machineTime);
+
+const bookFrom1919 = library.findBookBy("releaseDate", 1919);
+if (bookFrom1919 === null) {
+  library.addBook(new Book(bookFrom1919));
+}
+
+const bookToRead = library.giveBookByName("Машина времени");
+bookToRead.state = 20;
+bookToRead.fix();
+library.addBook(bookToRead);
+console.log(bookToRead)
+const foundBook = library.giveBookByName( "Чистый код");
+console.log(foundBook)
